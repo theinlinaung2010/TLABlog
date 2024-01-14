5c222c8f7a79b2ec6e866f175f762f6f0e8737c1
Thein Lin Aung <theinlinaung2010@gmail.com>
2024-01-14 22:02:10 +0900

    added Github button and style update

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,7 @@
 
 import { jsx } from "theme-ui";
 import { Link } from "gatsby";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -9,18 +10,39 @@ const Footer = () => {
       sx={{
         width: "100%",
         variant: "layout.footer",
+        fontSize: "small",
       }}
     >
-      <div sx={{ mx: "auto", maxWidth: "container" }}>
-        TLABlog.&nbsp;CC BY-NC 4.0.&nbsp;
-        <a
-          href="https://creativecommons.org/licenses/by-nc/4.0/"
-          sx={{ color: "white" }}
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+        sx={{
+          mx: "auto",
+
+          height: "2rem",
+          maxWidth: "container",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <span sx={{ mx: [3, 2, 0] }}>
+            TLABlog.&nbsp;CC BY-NC 4.0.&nbsp;
+            <a
+              href="https://creativecommons.org/licenses/by-nc/4.0/"
+              target="_blank"
+              rel="noopener noreferrer"
+              sx={{ color: "white", "&:hover": { color: "accent" } }}
+            >
+              Some rights reserved.
+            </a>
+          </span>
+        </div>
+        <Link
+          to="https://github.com/theinlinaung2010/TLABlog"
+          sx={{ mx: [3, 2, 0], color: "white", display: ["none", "flex", "flex"], "&:hover": { color: "accent" } }}
         >
-          Some rights reserved.
-        </a>
+          <FaGithub size="1.2rem" />
+        </Link>
       </div>
     </footer>
   );
