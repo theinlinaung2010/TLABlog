34fc3faca01956d87be005bb657772286165076a
Thein Lin Aung <theinlinaung2010@gmail.com>
2024-03-06 21:45:40 +0900

    GA script added

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,6 +14,16 @@ export default (props) => (
   >
     <Helmet>
       <title>{props.title}</title>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-47HZ7B9QEG"></script>
+      <script>
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-47HZ7B9QEG');`}
+      </script>
     </Helmet>
     <Header></Header>
     <div sx={{ flexGrow: 999 }}>{props.children}</div>
